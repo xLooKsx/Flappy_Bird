caa9a65b5a304e7fe46420c3b6e05354df6c9d37
xLooKsx <lu.oliveira.silva98@gmail.com>
2024-08-28 07:42:52 +0100

    feat: Score implementation and Obstacle  - Score implemented  - obstacle height random

--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -1,16 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
     public GameObject GameOverPannel;
+    public Text score;
     void Start()
     {
+        score.text = "0";
     }
 
     public void ShowGameOverPanel(){
 
         this.GameOverPannel.SetActive(true);
+    }
+
+    public void UpdateScoreText(String newScore){
+        score.text = newScore;
     }
 }
